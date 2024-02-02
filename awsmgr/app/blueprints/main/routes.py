6c812b330fccc1ac8db26d6992272be5acf0c405
Hagan Franks <224019+hagan@users.noreply.github.com>
2024-02-02 11:53:35 -0700

    disabled main view

--- awsmgr/app/blueprints/main/routes.py
+++ awsmgr/app/blueprints/main/routes.py
@@ -3,14 +3,14 @@ from flask import render_template, current_app
 from awsmgr.app.blueprints.main import bp
 
 
-@bp.route('/')
-def index():
-    getfromconf = lambda X: current_app.config[X] if X in current_app.config else ''
-    context = {
-        # 'PUBLIC_KEY': getfromconf('PUBLIC_KEY'),
-        # 'BEARER_TOKEN': getfromconf('BEARER_TOKEN'),
-        # 'APPSTREAM_API': getfromconf('APPSTREAM_API'),
-        # 'APPSTREAM_LAMBDA_ROOT_URL': getfromconf('APPSTREAM_LAMBDA_ROOT_URL'),
-        # 'APPSTREAM_STREAMING_URL': getfromconf('APPSTREAM_STREAMING_URL'),
-    }
-    return render_template('index.html', **context)
+# @bp.route('/')
+# def index():
+#     getfromconf = lambda X: current_app.config[X] if X in current_app.config else ''
+#     context = {
+#         # 'PUBLIC_KEY': getfromconf('PUBLIC_KEY'),
+#         # 'BEARER_TOKEN': getfromconf('BEARER_TOKEN'),
+#         # 'APPSTREAM_API': getfromconf('APPSTREAM_API'),
+#         # 'APPSTREAM_LAMBDA_ROOT_URL': getfromconf('APPSTREAM_LAMBDA_ROOT_URL'),
+#         # 'APPSTREAM_STREAMING_URL': getfromconf('APPSTREAM_STREAMING_URL'),
+#     }
+#     return render_template('index.html', **context)
